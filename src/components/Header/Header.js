09d972c89340e6f7ce5ffa9662bf66825d364f57
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-14 18:54:51 -0300

    exercicios-concluidos

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "./styles";
 
-const Header = () => {
+const Header = ({id, setId, nome, setNome, tipo, setTipo, ordenar, setOrdenar}) => {
   const pokemontypesArray = [
     "Normal",
     "Fire",
@@ -23,17 +23,43 @@ const Header = () => {
     "Fairy",
   ];
 
+  const onChangeId = (event) => {
+    setId(event.target.value)
+  }
+
+  const onChangeNome = (event) => {
+    setNome(event.target.value)
+  }
+
+  const onChangeTipo = (event) => {
+    setTipo(event.target.value)
+  }
+
+  const onChangeOrdenar = (event) => {
+    setOrdenar(event.target.value)
+  }
+
+
+  
+
+
+
   return (
     <Container>
-      <input type="number" placeholder="Buscar por id" />
-      <input type="text" placeholder="Buscar por nome" />
-      <select>
+      <input type="number" placeholder="Buscar por id" onChange={(event)=> onChangeId(event)}/>
+      <input type="text" placeholder="Buscar por nome" onChange={(event)=> onChangeNome(event)}/>
+
+      <select onChange={(event)=> onChangeOrdenar(event)}>
         <option value="">Ordenar</option>
-        <option value="">Crescente</option>
-        <option value="">Decrescente</option>
+        <option value="Crescente">Crescente</option>
+        <option value="Decrescente">Decrescente</option>
       </select>
-      <select name="tipo" id="tipo">
-        <option value="">Selecione um tipo</option>
+
+      
+      <select value={""} onChange={(event) =>
+        onChangeTipo(event)} name="tipo" id="tipo">
+        <option value="" >Selecione um tipo</option>
+
         {pokemontypesArray.map((type) => {
           return (
             <option key={type} value={type}>
